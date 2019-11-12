27a9945a5dfa9e0b2de101fdfa96a2863ddbed3b
Raza Poonja <razapoonja@gmail.com>
2019-11-12 23:58:42 +0500

    Continued decision tree regression

---
+++ decision_tree_regression.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeRegressor
+
+dataset = pd.read_csv('position_salaries.csv')
+X = dataset.iloc[:, 1:2].values
+Y = dataset.iloc[:, 2].values
+
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, Y)
+
+y_pred = regressor.predict(np.array([[6.5]]))
+print(y_pred)
+
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, Y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
